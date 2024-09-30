e83733f5bc63b5df4bd62e1e3a49dfb6bb2f8445
itsdorosh <andry.dorosh@gmail.com>
2024-09-30 19:58:08 +0300

    init module building and serving via webpack

---
+++ src/main.js
@@ -0,0 +1,3 @@
+import './style.css';
+
+document.getElementById("root").innerHTML = "<h1>Greeting the World!</h1>";
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { title } = require("process");
+
+module.exports = (env, argv) => {
+    const isDevelopment = argv.mode === "development";
+
+    return {
+        entry: "./src/main.js",
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: isDevelopment ? '[name].js' : '[name].[contenthash].js',
+        },
+        // devtool: "eval-sourse-map",
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: "/node_modules/",
+                    use: {
+                        loader: "babel-loader"
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    }]
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./src/index.html",
+                templateParameters: {
+                    title: "Shapeshifter 3D Module Project"
+                },
+            })
+        ]
+    }
+};
